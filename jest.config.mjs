9c86c96047f383ed9fb47eb54b9564b750f571d6
Juan Carlos Mordan <juan.mordan@gmail.com>
2025-10-15 17:18:02 -0400

    chore: fix jest config linting

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,10 +1,14 @@
-const { defaults } = require('ts-jest')
-const { pathsToModuleNameMapper } = require('ts-jest')
-const { compilerOptions } = require('./tsconfig.json')
+// const { defaults } = require('ts-jest')
+// const { pathsToModuleNameMapper } = require('ts-jest')
+// const { compilerOptions } = require('./tsconfig.json')
+
+import tsJest from 'ts-jest'
+import { pathsToModuleNameMapper } from 'ts-jest'
+import tsconfig from './tsconfig.json' with { type: 'json' }
 
 /** @type {import('jest').Config} */
-module.exports = {
-  ...defaults,
+export default {
+  ...tsJest.defaults,
   testEnvironment: 'node',
   testMatch: ['**/*.spec.ts'],
   coveragePathIgnorePatterns: ['/node_modules/', '/dist/', 'src/__tests__/'],
@@ -15,7 +19,7 @@ module.exports = {
   },
   moduleFileExtensions: ['ts', 'js', 'json'],
   collectCoverageFrom: ['src/**/*.ts', '!src/**/*.d.ts'],
-  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+  moduleNameMapper: pathsToModuleNameMapper(tsconfig.compilerOptions.paths, {
     prefix: '<rootDir>/',
   }),
   detectOpenHandles: true,
