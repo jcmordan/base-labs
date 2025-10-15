a1acce03b9010d91bfb1671eaad9d396fda1a473
Juan Carlos Mordan <juan.mordan@gmail.com>
2025-10-15 14:34:25 -0400

    chore: add jest config

---
+++ jest.config.cjs
@@ -0,0 +1,21 @@
+const { defaults } = require('ts-jest')
+const { pathsToModuleNameMapper } = require('ts-jest')
+const { compilerOptions } = require('./tsconfig.json')
+
+/** @type {import('jest').Config} */
+module.exports = {
+  ...defaults,
+  testEnvironment: 'node',
+  testMatch: ['**/*.spec.ts'],
+  coveragePathIgnorePatterns: ['/node_modules/', '/dist/'],
+  setupFilesAfterEnv: [],
+  preset: 'ts-jest',
+  transform: {
+    '^.+\\.ts$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'json'],
+  collectCoverageFrom: ['src/**/*.ts', '!src/**/*.d.ts'],
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: '<rootDir>/',
+  }),
+}
