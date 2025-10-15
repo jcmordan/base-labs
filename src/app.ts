82f6a328db768bd40e984c39e59f47a5c16b4ec9
Juan Carlos Mordan <juan.mordan@gmail.com>
2025-10-15 15:52:55 -0400

    feat(api): add rate limiter

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 import cornRouter from '@/routes/cornRoutes'
+import rateLimit from 'express-rate-limit'
 
 const createApp = (): express.Application => {
   const app = express()
@@ -15,7 +16,15 @@ const createApp = (): express.Application => {
     })
   })
 
-  app.use('/corn', cornRouter)
+  const limiter = rateLimit({
+    windowMs: 1 * 60 * 1000, // 1 minutes
+    max: 1, // limit each IP to 1 requests per
+    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+    legacyHeaders: false, // Disable the `X-Rate-Limit-*` headers
+  })
+
+  // Apply rate limit to corn routes
+  app.use('/corn', limiter, cornRouter)
 
   return app
 }
